41d69bb43c52876333bb423707f2a76617ff2d55
behweifeng97 <behweifeng97@gmail.com>
2020-03-26 09:35:53 +0800

    register account will create a new user settings into db cannot retrieve any info yet

--- app/src/main/java/com/example/myapplication/SettingsActivity.kt
+++ app/src/main/java/com/example/myapplication/SettingsActivity.kt
@@ -1,16 +1,40 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseReference
 
 class SettingsActivity : AppCompatActivity() {
+
+    private val TAG = "SettingsActivity"
+    private lateinit var mUser: UserSettings
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDatabase: DatabaseReference
+
+    fun DataSnapshot.asUser(): UserSettings? =
+        getValue(UserSettings::class.java)?.copy(email = key)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_page_2)
         actionBar?.setDisplayHomeAsUpEnabled(true)
+
+//        mAuth = FirebaseAuth.getInstance()
+//        mDatabase = FirebaseDatabase.getInstance().reference
+//
+//        fun currentUserReference(): DatabaseReference =
+//            mDatabase.child("Users").child(mAuth.currentUser!!.email)
+//
+//        currentUserReference().addListenerForSingleValueEvent(){
+//            ValueListenerAdapter{
+//                mUser = it.asUser()!!
+//                loginEmail.setText(mUser.email)
+//            }
+//        }
     }
 
     fun saveSettings(view: View){
---
+++ app/src/main/java/com/example/myapplication/utils/ValueListenerAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication.utils
+
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+
+class ValueListenerAdapter(val handler: (DataSnapshot) -> Unit): ValueEventListener {
+
+    private val TAG = "ValueListenerAdapter"
+
+    override fun onCancelled(error: DatabaseError) {
+        Log.e("onCancelled", TAG, error.toException())
+    }
+
+    override fun onDataChange(data: DataSnapshot) {
+        handler(data)
+    }
+
+}
